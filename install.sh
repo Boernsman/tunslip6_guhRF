c6f449d948d6d42e13abcc70054f8f57a9f95dad
Boernsman <bernhard.trinnes@guh.guru>
2015-12-28 00:50:04 +0100

    Initial Commit

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+sudo wget --no-check-certificate https://raw.githubusercontent.com/osdomotics/osd-contiki/master/tools/tunslip6.c
+sudo gcc tunslip6.c -o tunslip6
+sudo chmod 766 tunslip6
+sudo cp tunslip6 /usr/sbin/
+sudo cp sbin/tunslip6.sh /usr/sbin/tunslip6.sh						#git link
+cd /usr/sbin/
+sudo chmod 766 tunslip6.sh
